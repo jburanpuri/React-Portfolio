50b9b5cea29c71b2417005ec96d4f18b12d04e44
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-09-08 11:37:24 -0400

    fixed wording, added more information

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,44 +15,44 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h3>Education</h3>
-              <small>Bachelors in Software Engineering Science</small>
+              <small>Bachelors in Software Engineering Science from Western University</small>
             </article>
 
             <article className='about__card'>
               <FiClipboard className='about__icon' />
               <h3>Technical Skills</h3>
-              <small>Experienced in handful languages, libraries & frameworks</small>
+              <small>Experienced in handful of languages, libraries & frameworks. Always excited to learn more!</small>
             </article>
 
             <article className='about__card'>
               <FiUsers className='about__icon' />
               <h3>Teamwork Driven</h3>
-              <small>Experienced in using agile (SCRUM) methodology for sprints</small>
+              <small>Experienced using agile (SCRUM) methodology for sprints</small>
             </article>
 
             <article className='about__card'>
               <FiGithub className='about__icon' />
               <h3>Projects</h3>
-              <small>Extensive list of unique projects on GitHub</small>
+              <small>Extensive list of projects on GitHub</small>
             </article>
 
             <article className='about__card'>
               <FiBarChart className='about__icon' />
-              <h3>Optimization</h3>
-              <small>Ability in optimizing algorithms for time & space Complexity</small>
+              <h3>Always Growing</h3>
+              <small>Life long learning of new technical and non-technical skills</small>
             </article>
 
 
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h3>Experience</h3>
-              <small>Comfortable in working in fast paced teams</small>
+              <small>Comfortable working in fast paced teams and environments</small>
             </article>
           </div>
 
           <center> <p>
             Junior software engineer with a passion for developing innovative and useful solutions.
-            A strong passion for designing, developing, and testing code.
+            I have strong passion for designing, developing, and testing code. Growing interest in machine learning, AI, and optimizing algorithms and data structures to reduce both space and time complexities.
             I am always on the lookout for new software opportunities so please don't hesitate to contact me!
           </p></center>
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} title='About Me'><AiOutlineUser /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} title='My Projects'><BiBook /></a>
       <a href="#experiences" onClick={() => setActiveNav('#experiences')} className={activeNav === '#experiences' ? 'active' : ''} title='My Experiences'><BsCardChecklist /></a>
-      <a href="#technology" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} title='Technology'><AiFillFund /></a>
+      <a href="#technology" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} title='Technologies'><AiFillFund /></a>
       <a href="#contact" on onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} title='Contact Me'><BiMessageSquareDetail /></a>
     </nav>
   )
