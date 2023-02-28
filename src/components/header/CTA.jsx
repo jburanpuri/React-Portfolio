1d421e6f0c328d8f203e756d4c4d4b5bf6fdeadd
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-02-28 18:33:52 -0500

    Updated portfolio site

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Resume from '../../assets/resume/resume.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={Resume} target="_blank" className='btn btn-primary'>Open CV</a>
       <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
